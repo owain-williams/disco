305595666c18b2a7c46222b4b5179e665de57d94
Owain Williams <owain@andersdigital.com>
2025-04-06 13:38:44 +0100

    add dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -21,7 +22,16 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en">
-        <body className={`${openSans.variable} antialiased`}>{children}</body>
+        <body className={`${openSans.variable} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </Providers>
   );
