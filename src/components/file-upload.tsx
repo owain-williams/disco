2040ed4a07f3db7ffa1b3655c23ee1ccdc0b05bb
Owain Williams <owain@andersdigital.com>
2025-04-08 19:41:36 +0100

    add uploads

---
+++ src/components/file-upload.tsx
@@ -0,0 +1,47 @@
+"use-client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+type FileUploadProps = {
+  endpoint: "messageFile" | "serverImage";
+  value: string;
+  onChange: (url: string) => void;
+};
+
+export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-red-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div className="space-y-4">
+      <UploadDropzone
+        appearance={{
+          uploadIcon: "size-12 my-4 text-muted-foreground",
+          button: "bg-primary text-primary-foreground px-4 my-4",
+        }}
+        endpoint={endpoint}
+        onClientUploadComplete={(res) => {
+          onChange(res[0].ufsUrl);
+        }}
+        onUploadError={(error: Error) => {
+          console.log(error);
+        }}
+      />
+    </div>
+  );
+};
--- src/components/modals/initial-modal.tsx
+++ src/components/modals/initial-modal.tsx
@@ -22,6 +22,7 @@ import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Server name is required" }),
@@ -59,7 +60,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                {/* TODO: Image Upload */}
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
