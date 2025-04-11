88432d2b206a9d1e5b83679c927a66dcbfe37402
Owain Williams <owain@andersdigital.com>
2025-04-11 13:49:50 +0100

    add edit server modal

--- src/app/(main)/servers/[serverId]/page.tsx
+++ src/app/(main)/servers/[serverId]/page.tsx
@@ -1,13 +1,7 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-import { SignOutButton } from "@clerk/nextjs";
-
 export const ServerIdPage = () => {
   return (
     <div className="h-full">
-      <div className="flex flex-col items-center justify-center h-full">
-        <SignOutButton />
-        <ThemeToggle />
-      </div>
+      <div className="flex flex-col items-center justify-center h-full"></div>
     </div>
   );
 };
