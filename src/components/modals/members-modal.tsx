9e7784e1bac62697aac371bc550f3e92744f6ee1
Owain Williams <owain@andersdigital.com>
2025-04-11 14:56:35 +0100

    add members modal

---
+++ src/components/modals/members-modal.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/lib/types";
+import { ScrollArea } from "../ui/scroll-area";
+import { UserAvatar } from "../user-avatar";
+
+export const MembersModal = () => {
+  const { isOpen, onClose, onOpen, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "members";
+  const { server } = data as { server: ServerWithMembersWithProfiles };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text black overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-black font-bold text-center">
+            Manage Members
+          </DialogTitle>
+          <DialogDescription className="text-center text-muted">
+            {server?.members?.length} Members
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+            </div>
+          ))}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
